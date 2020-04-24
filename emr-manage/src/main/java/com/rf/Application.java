c559568874d0e9356e1f8c5075ee6a43f6034a35
Mei Li（开发_梅利） <meili@111.com.cn>
2020-04-24 19:06:16 +0800

    init

--- emr-manage/src/main/java/com/rf/Application.java
+++ emr-manage/src/main/java/com/rf/Application.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
  * author: MeiLi
  */
 @SpringBootApplication
-@ServletComponentScan(basePackages = "com.rf")
+@ComponentScan(basePackages = "com.rf.*")
 public class Application {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
